ce47080a9f236b657b670f874327b8564e07b865
Rachel Lew <rlew07@gmail.com>
2019-10-11 09:33:14 -0600

    Adds astronaut objects

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# astronauts
+neil_armstrong = Astronaut.create(name: "Neil Armstrong", age: 37, job: "Commander")
+buzz_aldrin = Astronaut.create(name: "Buzz Aldrin", age: 35, job: "Pilot")
+michael_collins = Astronaut.create(name: "Michael Collins", age: 32, job: "Test Pilot")
