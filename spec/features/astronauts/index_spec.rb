da02330eaaf12923625c50ece84159f63133d42a
Rachel Lew <rlew07@gmail.com>
2019-10-11 10:27:53 -0600

    Adds astronauts to missions in test

--- spec/features/astronauts/index_spec.rb
+++ spec/features/astronauts/index_spec.rb
@@ -28,14 +28,26 @@ RSpec.describe "astronaut index page" do
   end
 
   it "I can see a list of the space missions in alphabetical order for each astronaut" do
+    apollo_13 = Mission.create!(title: "Apollo 13", time_in_space: 300)
+    capricorn_4 = Mission.create!(title: "Capricorn 4", time_in_space: 400)
+    gemini_7 = Mission.create!(title: "Gemini 7", time_in_space: 500)
+    voyager_1 = Mission.create!(title: "Voyager 1", time_in_space: 600)
+    kepler_mission = Mission.create!(title: "Kepler Mission", time_in_space: 800)
+
     neil_armstrong = Astronaut.create(name: "Neil Armstrong", age: 37, job: "Commander")
     buzz_aldrin = Astronaut.create(name: "Buzz Aldrin", age: 35, job: "Pilot")
     michael_collins = Astronaut.create(name: "Michael Collins", age: 32, job: "Test Pilot")
-    apollo_13 = Mission.create(title: "Apollo 13", time_in_space: 300)
-    capricorn_4 = Mission.create(title: "Capricorn 4", time_in_space: 400)
-    gemini_7 = Mission.create(title: "Gemini 7", time_in_space: 500)
-    voyager_1 = Mission.create(title: "Voyager 1", time_in_space: 600)
-    kepler_mission = Mission.create(title: "Kepler Mission", time_in_space: 800)
+
+    apollo_13.astronauts << neil_armstrong
+    apollo_13.astronauts << michael_collins
+    capricorn_4.astronauts << neil_armstrong
+    capricorn_4.astronauts << buzz_aldrin
+    gemini_7.astronauts << buzz_aldrin
+    gemini_7.astronauts << michael_collins
+    kepler_mission.astronauts << neil_armstrong
+    kepler_mission.astronauts << buzz_aldrin
+    voyager_1.astronauts << michael_collins
+
     visit '/astronauts'
 
     within "#astronaut-#{neil_armstrong.id}" do
